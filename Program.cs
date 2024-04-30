9858caedfb65dd298294f78d069bbb0f82217b11
Ksenia Solodovnikova <ksenia@nalunch.ru>
2024-04-30 10:47:37 +0300

    added version

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => { return "Timeweb Cloud + .NET Core = ❤️"; });
+app.MapGet("/version", () => { return "1.0.0.0"; });
 
 app.Run();
